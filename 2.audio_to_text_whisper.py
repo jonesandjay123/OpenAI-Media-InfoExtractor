6652d41ce26e83e28004f418fb862227880811d2
joneswang <jonescwang@gmail.com>
2023-04-06 00:08:40 -0400

    update wisper.py

--- 2.audio_to_text_whisper.py
+++ 2.audio_to_text_whisper.py
@@ -1,37 +1,24 @@
-import os
-import sys
 import whisper
-from pathlib import Path
+import os
 
-model = whisper.load_model("base")
-
-def transcribe_audio_with_progress(file_path):
-
-    # Load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio(file_path)
-    audio = whisper.pad_or_trim(audio)
-
-    # Make log-Mel spectrogram and move to the same device as the model
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    options = whisper.DecodingOptions()
-
-    # Call whisper.decode()
-    result = whisper.decode(model, mel, options)
-
-    return result.text
+def transcribe_audio(file_path):
+    model = whisper.load_model("base")
+    result = model.transcribe(file_path)
+    return result["text"]
 
 def main():
-    file_path = "audio/sample.mp3"
-    output_path = Path(file_path).with_suffix('.txt')
-
+    file_path = 'audio/sample.mp3'
+    output_dir = 'output'
+    output_filename = 'output_transcript.txt'
+    output_path = os.path.join(output_dir, output_filename)
     print("Transcribing audio...")
-    transcription = transcribe_audio_with_progress(file_path)
+    transcription = transcribe_audio(file_path)
+    print("\nTranscription:\n", transcription)
 
-    with open(output_path, "w") as output_file:
-        output_file.write(transcription)
+    with open(output_path, 'w') as f:
+        f.write(transcription)
 
-    print(f"Transcription saved to {output_path}")
+    print("Transcription saved to", output_path)
 
 if __name__ == "__main__":
     main()
