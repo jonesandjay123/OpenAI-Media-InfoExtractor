2948c1b3c62826b2e2353da8ecc0a7a69382fdea
joneswang <jonescwang@gmail.com>
2023-04-06 14:16:26 -0400

    first working version of decode with progress bar

--- 2.audio_to_text_whisper.py
+++ 2.audio_to_text_whisper.py
@@ -22,8 +22,14 @@ num_segments = int(np.ceil(total_duration / segment_duration))
 # 初始化一個空字符串，用於存儲所有片段的識別結果
 full_transcript = ""
 
-# 創建解碼選項
-options = whisper.DecodingOptions(fp16=False)
+# 在循環外檢測語言
+audio_segment = audio[:segment_duration * 16000]
+mel = whisper.log_mel_spectrogram(audio_segment).to(model.device)
+_, probs = model.detect_language(mel)
+language = max(probs, key=probs.get)
+
+# 創建解碼選項並指定語言
+options = whisper.DecodingOptions(fp16=False, language=language)
 
 # 分段處理音頻並添加進度條
 for i in tqdm.tqdm(range(num_segments)):
@@ -32,6 +38,10 @@ for i in tqdm.tqdm(range(num_segments)):
 
     # 裁剪音頻片段
     audio_segment = audio[start:end]
+
+    # 確保音頻片段具有正確的形狀
+    if audio_segment.shape[0] < segment_duration * 16000:
+        audio_segment = np.pad(audio_segment, (0, segment_duration * 16000 - audio_segment.shape[0]))
 
     # 計算對數梅爾頻譜圖並將其移動到與模型相同的設備上
     mel = whisper.log_mel_spectrogram(audio_segment).to(model.device)
