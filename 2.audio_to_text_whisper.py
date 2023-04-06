648452f73df449d10fca748b62653d0e8d4a0398
joneswang <jonescwang@gmail.com>
2023-04-06 12:27:19 -0400

    save place

--- 2.audio_to_text_whisper.py
+++ 2.audio_to_text_whisper.py
@@ -1,25 +1,46 @@
 import torch
 import whisper
 import os
+import tqdm
+import numpy as np
 
 # 載入模型
-model = whisper.load_model("tiny")  # tiny, base, small, medium, large
+model = whisper.load_model("tiny")
 
-# 讀取音頻檔案並將其裁剪或填充為30秒
+# 讀取音頻檔案
 audio = whisper.load_audio('audio/sample.mp3')
-audio = whisper.pad_or_trim(audio)
 
-# 計算對數梅爾頻譜圖並將其移動到與模型相同的設備上
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+# 計算音頻的總時間（秒）
+total_duration = len(audio) / 16000
+
+# 設定每個片段的持續時間（秒）
+segment_duration = 30
+
+# 計算需要多少個片段來處理整個音頻
+num_segments = int(np.ceil(total_duration / segment_duration))
+
+# 初始化一個空字符串，用於存儲所有片段的識別結果
+full_transcript = ""
 
 # 創建解碼選項
 options = whisper.DecodingOptions(fp16=False)
 
-# 使用解碼函數
-result = whisper.decode(model, mel, options)
+# 分段處理音頻並添加進度條
+for i in tqdm.tqdm(range(num_segments)):
+    start = i * segment_duration * 16000
+    end = (i + 1) * segment_duration * 16000
 
-# 打印識別出的文本
-print(result.text)
+    # 裁剪音頻片段
+    audio_segment = audio[start:end]
+
+    # 計算對數梅爾頻譜圖並將其移動到與模型相同的設備上
+    mel = whisper.log_mel_spectrogram(audio_segment).to(model.device)
+
+    # 使用解碼函數
+    result = whisper.decode(model, mel, options)
+
+    # 將片段的識別結果添加到完整文本中
+    full_transcript += result.text + " "
 
 # 確保 output 資料夾存在
 if not os.path.exists('output'):
@@ -27,4 +48,4 @@ if not os.path.exists('output'):
 
 # 寫入 output_transcript.txt 檔案
 with open('output/output_transcript.txt', 'w') as file:
-    file.write(result.text)
+    file.write(full_transcript)
