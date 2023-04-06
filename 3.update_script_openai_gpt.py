6783c1afdf94cc395cf7fde99d982d96ba6a7687
joneswang <jonescwang@gmail.com>
2023-04-06 18:34:02 -0400

    token limitation still needs to be fixed

--- 3.update_script_openai_gpt.py
+++ 3.update_script_openai_gpt.py
@@ -1,4 +1,7 @@
 import openai
+import os
+
+language = "zh"  # Use "en" for English and "zh" for Chinese
 
 # Read API key from file
 with open("api_key.txt", "r") as f:
@@ -7,23 +10,32 @@ with open("api_key.txt", "r") as f:
 # Print the API key to verify it's being read correctly
 print(f"API Key: {openai.api_key}")
 
-# 讀取轉錄文本
-with open("output/output_transcript.txt", "r") as file:
-    transcript = file.read()
+output_dir = "output"
 
-# 調用GPT-3.5，提取重點信息並生成摘要
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Please summarize the following transcript into bullet points by traditional chinese: {transcript}"}
-    ]
-)
+def summarize_file(file_path):
+    # 讀取轉錄文本
+    with open(file_path, "r") as file:
+        transcript = file.read()
 
-# 獲取摘要
-summary = response.choices[0].message['content']
+    # 調用GPT-3.5，提取重點信息並生成摘要
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"Please summarize the following transcript into bullet points{' by traditional chinese' if language == 'zh' else ''}: {transcript}"}
+        ]
+    )
 
-# 將摘要添加到原始轉錄文件中
-with open("output/output_transcript.txt", "a") as file:
-    file.write("\n------\n\nSummary:\n\n")
-    file.write(summary)
+    # 獲取摘要
+    summary = response.choices[0].message['content']
+
+    # 將摘要添加到原始轉錄文件中
+    with open(file_path, "a") as file:
+        file.write("\n------\n\nSummary:\n\n")
+        file.write(summary)
+
+# Loop through all files in the output directory and summarize them
+for file_name in os.listdir(output_dir):
+    if file_name.startswith("output_transcript"):
+        file_path = os.path.join(output_dir, file_name)
+        summarize_file(file_path)
